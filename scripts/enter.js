0626179ef2882417298df4502da31a8c584b5946
Michal <michal.kacir@gmail.com>
2023-05-21 21:36:20 +0200

    spaces fix

--- scripts/enter.js
+++ scripts/enter.js
@@ -14,6 +14,6 @@ async function enterRaffle() {
 enterRaffle()
   .then(() => process.exit(0))
   .catch((error) => {
-      console.error(error);
-      process.exit(1);
+    console.error(error);
+    process.exit(1);
   });
